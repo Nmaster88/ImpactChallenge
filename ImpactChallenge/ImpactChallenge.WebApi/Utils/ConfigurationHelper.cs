df9be5f2c190dfa2ee3a9c4ad19cf6f48145ac68
Nmaster88 <nunomaster21@gmail.com>
2023-07-14 21:31:09 +0100

    more info

--- ImpactChallenge/ImpactChallenge.WebApi/Utils/ConfigurationHelper.cs
+++ ImpactChallenge/ImpactChallenge.WebApi/Utils/ConfigurationHelper.cs
@@ -3,6 +3,8 @@
     public interface IConfigurationHelper
     {
         string BasketApiUrl { get; }
+        int CircuitBreakerExceptionsLimit { get; }
+        int CircuitBreakerDurationOfBreak { get; }
     }
 
     public class ConfigurationHelper : IConfigurationHelper
@@ -12,8 +14,12 @@
             _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
 
             BasketApiUrl = configuration.GetSection("AppSettings").GetValue<string>("BasketApiUrl");
+            CircuitBreakerExceptionsLimit = configuration.GetSection("AppSettings").GetValue<int>("CircuitBreaker.ExceptionsLimit");
+            CircuitBreakerDurationOfBreak = configuration.GetSection("AppSettings").GetValue<int>("CircuitBreaker.DurationOfBreak");
         }
 
         public string BasketApiUrl { get; }
+        public int CircuitBreakerExceptionsLimit { get; }
+        public int CircuitBreakerDurationOfBreak { get; }
     }
 }
